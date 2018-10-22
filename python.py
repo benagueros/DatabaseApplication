ba779f43f480315f40da6138e0863be39e75bf2c
Benjamin Agueros <benagueros@gmail.com>
2018-10-22 14:40:05 -0500

    Created all tables with keys and clean properly

--- python.py
+++ python.py
@@ -10,13 +10,18 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE Experiment (ExperimentID VARCHAR(255) PRIMARY KEY, ManagerID CHAR(6), startDate DATE, DataEntryDate DATE)")
+mycursor.execute("CREATE TABLE Experiment(ExperimentID VARCHAR(255) PRIMARY KEY, ManagerID CHAR(6), startDate DATE, DataEntryDate DATE)")
 
 
 #in this table, need to set constraints on Type
-mycursor.execute("CREATE TABLE ParametersTypes (ExperimentID VARCHAR(255) FOREIGN KEY REFERENCES Experiment(ExperimentID), ParameterName VARCHAR(255) PRIMARY KEY, Type VARCHAR(255), Required BOOLEAN)")
+mycursor.execute("CREATE TABLE ParametersTypes(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), ParameterName VARCHAR(255) PRIMARY KEY, Type VARCHAR(255), Required BOOLEAN)")
 
-mycursor.execute("CREATE TABLE ResultTypes (ExperimentID VARCHAR(255) FOREIGN KEY REFERENCES Experiment(ExperimentID), ")
+#in this table, need to set constraints on Type
+mycursor.execute("CREATE TABLE ResultTypes(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), ResultName VARCHAR(255) PRIMARY KEY, Type VARCHAR(255), Required BOOLEAN)")
 
+mycursor.execute("CREATE TABLE Runs(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), TimeOfRun DATETIME PRIMARY KEY, ExperimentSSN CHAR(6), Success BOOLEAN)")
 
+mycursor.execute("CREATE TABLE RunsParameter(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), TimeOfRun DATETIME, ParameterName VARCHAR(255), Value VARCHAR(255), KEY(TimeOfRun, ParameterName))")
+
+mycursor.execute("CREATE TABLE RunsResult(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), TimeOfRun DATETIME, ResultName VARCHAR(255), Value VARCHAR(255), KEY(TimeOfRun, ResultName))")
 
