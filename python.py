5a485b1b5c989077b61c3a8d421d6e69d15a02f6
Benjamin Agueros <benagueros@gmail.com>
2018-10-22 09:00:17 -0500

    continued python database practice

--- python.py
+++ python.py
@@ -3,8 +3,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="Janaifloyd0209!"
+        passwd="Janaifloyd0209!",
+        database="mydatabase"
 )
 
-print(mydb)
 
+# mycursor = mydb.cursor()
+
+# mycursor.execute("SHOW DATABASES")
+
+# for x in mycursor:
+#    print(x)
