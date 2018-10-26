aa6f2e5818da0eb13fc69eac23b497b7f8eafdb0
benagueros <benagueros@gmail.com>
2018-10-26 09:54:18 -0500

    Stated input handling for experiment, need to verify and store.

--- python.py
+++ python.py
@@ -2,9 +2,7 @@ import mysql.connector
 
 def buildTables(mycursor):
 	mycursor.execute("CREATE TABLE Experiment(ExperimentID VARCHAR(255) PRIMARY KEY, ManagerID CHAR(6), startDate DATE, DataEntryDate DATE)")
-	#in this table, need to set constraints on Type
 	mycursor.execute("CREATE TABLE ParametersTypes(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), ParameterName VARCHAR(255), Type VARCHAR(255) CHECK (Type IN ('INT', 'FLOAT', 'STRING', 'URL', 'DATE', 'DATETIME')), Required BOOLEAN, KEY(ExperimentID, ParameterName))")
-	#in this table, need to set constraints on Type
 	mycursor.execute("CREATE TABLE ResultTypes(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), ResultName VARCHAR(255), Type VARCHAR(255) CHECK (Type IN ('INT', 'FLOAT', 'STRING', 'URL', 'DATE', 'DATETIME')), Required BOOLEAN, KEY(ExperimentID, ResultName))")
 	mycursor.execute("CREATE TABLE Runs(ExperimentID VARCHAR(255), FOREIGN KEY(ExperimentID) REFERENCES Experiment(ExperimentID), TimeOfRun DATETIME, ExperimentSSN CHAR(6), Success BOOLEAN, KEY(ExperimentID, TimeOfRun))")
 	mycursor.execute("CREATE TABLE RunsParameter(ExperimentID VARCHAR(255), TimeOfRun DATETIME, FOREIGN KEY(ExperimentID, TimeOfRun) REFERENCES Runs(ExperimentID, TimeOfRun), ParameterName VARCHAR(255), FOREIGN KEY(ExperimentID, ParameterName) REFERENCES ParametersTypes(ExperimentID, ParameterName),Value VARCHAR(255), KEY(ExperimentID, TimeOfRun, ParameterName))")
@@ -37,4 +35,32 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 buildTables(mycursor)
 destroyTables(mycursor)
+
+while True:
+	print("What would you like to do?")
+	print("1. Experiment entry")
+	print("2. Run Entry")
+	choice = int(input(""))
+	if choice != 1 and choice != 2:
+		print("Invalid Choice")
+		print("")
+	else:
+		break
+
+print("")
+if choice == 1:
+	ExperimentID = input("Enter ExperimentID: ")
+	ManagerID = input("Enter ManagerID: ")
+	StartDate = input("Enter StartDate: ")
+	DataEntryDate = input("Enter DataEntryDate: ")
+	parameters = int(input("How many parameters are there? "))
+	#need to verify and enter the data into the table before moving on to the next one	
+	while parameters > 0:
+		ParameterName = input("Enter ParameterName: ")
+		Type = input("Enter Type: ")
+		Required = input("Is It Required? (y/n):  ")
+		#need to verify the informaiton and store it in the tables before moving on to the next one
+		parameters-=1
+
+	
 
